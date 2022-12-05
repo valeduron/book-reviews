46dc984c6573d6b0e35a7d6b3f10b6f00113dc06
vduron <vduron@miners.utep.edu>
2022-12-04 22:03:07 -0700

    Added code for checking if database exists, adding it if it does not

--- Database/bookstoredb_dbo.books.sql
+++ Database/bookstoredb_dbo.books.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE  IF NOT EXISTS `bookstoredb` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bookstoredb`;
+
 --
 -- Table structure for table `dbo.books`
 --
